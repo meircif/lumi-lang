7d630dc37ec2286a8c4a1e0f7b750cbb60004bda
Meir Cohen <meircif@gmail.com>
2017-02-14 12:52:59 +0200

    Better MRB input handle

--- mrb.c
+++ mrb.c
@@ -16,6 +16,7 @@ Returncode string_get(String* this, Int index, Char* ch);
 Returncode string_copy(String* this, String* source);
 Returncode string_append(String* this, Char ch);
 Returncode string_concat(String* this, String* ext);
+Returncode string_has(String* this, Char ch, Bool* found);
 
 Returncode array_length(Array* arr, Int* length);
 
@@ -23,22 +24,24 @@ Returncode sys_system(String* command, Int* status);
 
 
 Returncode func(Array* argv) {
-  Int length = 0;
-  CHECK(array_length(argv, &length))
-  if (length != 2) {
+  if (argv->length != 2) {
     CHECK(print(&(String){26, 25, "usage: mrb [MR file name]"}))
     return OK;
   }
   String* mr_file;
   if (1 < 0 || 1 >= argv->length) RAISE mr_file = ((String*)(argv->values)) + 1;
-  CHECK(string_length(mr_file, &length))
-  if (length < 6) {
+  if (mr_file->actual_length < 6) {
     CHECK(print(&(String){23, 22, "MR file name too short"}))
     return OK;
   }
-  if ((0) + (length - 4) > mr_file->actual_length) RAISE String* prefix = &(String){length - 4, length - 4, mr_file->chars + 0};
+  Bool has;
+  CHECK(string_has(mr_file, '"', &has)) if (has) {
+    CHECK(print(&(String){33, 32, "Illegal \" character in file name"}))
+    return OK;
+  }
+  if ((0) + (mr_file->actual_length - 4) > mr_file->actual_length) RAISE String* prefix = &(String){mr_file->actual_length - 4, mr_file->actual_length - 4, mr_file->chars + 0};
   Char version;
-  CHECK(string_get(mr_file, length - 4, &version))
+  CHECK(string_get(mr_file, mr_file->actual_length - 4, &version))
   if (version < '0' or version > '2') {
     CHECK(print(&(String){23, 22, "Unsupported MR version"}))
     return OK;
@@ -47,11 +50,11 @@ Returncode func(Array* argv) {
   String* command = &(String){128, 0, (char[128]){0}};
   CHECK(string_copy(command, &(String){3, 2, "mr"}))
   CHECK(string_append(command, version))
-  CHECK(string_concat(command, &(String){15, 14, "-compiler.exe "}))
+  CHECK(string_concat(command, &(String){16, 15, "-compiler.exe \""}))
   CHECK(string_concat(command, mr_file))
-  CHECK(string_append(command, ' '))
+  CHECK(string_concat(command, &(String){4, 3, "\" \""}))
   CHECK(string_concat(command, prefix))
-  CHECK(string_append(command, 'c'))
+  CHECK(string_concat(command, &(String){3, 2, "c\""}))
   
   Int status = 0;
   CHECK(print(command))
@@ -61,9 +64,9 @@ Returncode func(Array* argv) {
     return OK;
   }
   /* run C compiler */
-  CHECK(string_copy(command, &(String){5, 4, "gcc "}))
+  CHECK(string_copy(command, &(String){6, 5, "gcc \""}))
   CHECK(string_concat(command, prefix))
-  CHECK(string_concat(command, &(String){3, 2, "c "}))
+  CHECK(string_concat(command, &(String){4, 3, "c\" "}))
   if (version == '0') {
     CHECK(string_concat(command, &(String){24, 23, "mr0-file.c mr0-string.c"}))
   }
@@ -73,9 +76,9 @@ Returncode func(Array* argv) {
   if (version == '2') {
     CHECK(string_concat(command, &(String){7, 6, "mr.1.c"}))
   }
-  CHECK(string_concat(command, &(String){5, 4, " -o "}))
+  CHECK(string_concat(command, &(String){6, 5, " -o \""}))
   CHECK(string_concat(command, prefix))
-  CHECK(string_concat(command, &(String){4, 3, "exe"}))
+  CHECK(string_concat(command, &(String){5, 4, "exe\""}))
   CHECK(print(command))
   CHECK(sys_system(command, &status))
   if (status != 0) {
