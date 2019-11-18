d168543df3344528e70a95264f495d3e9d39a842
snudler6 <snudler6@gmail.com>
2019-11-18 21:07:19 +0200

    Validate the test values

--- stdlibs/tests/unistd.h
+++ stdlibs/tests/unistd.h
@@ -1,9 +1,11 @@
 
+#import <string.h>
+
 #define F_OK 0
 #define R_OK 1
 #define W_OK 2
 #define X_OK 3
 
 int access(char * path, int amode) {
-  return 0;
+  return (strcmp(path, "/some/path") == 0) && (amode == F_OK);
 }
